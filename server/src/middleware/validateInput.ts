ef46ef0c2a2adb4b5e13d7b0d68fe68cc93490c5
Evan Kasky <ekasky25@gmail.com>
2024-07-21 14:44:57 -0400

    Adds middleware to validate input given a schema

---
+++ server/src/middleware/validateInput.ts
@@ -0,0 +1,50 @@
+import { Request, Response, NextFunction } from 'express';
+import { z, ZodEffects, ZodError, ZodObject } from 'zod';
+import { StatusCodes } from 'http-status-codes';
+import { IAPIResponse } from '../types/IApiResponse';
+
+
+export function validateRequestInput(schema: z.ZodObject<any, any> | ZodEffects<ZodObject<any, any>>) {
+
+    return (req: Request, res: Response, next: NextFunction): void => {
+
+        try {
+
+            schema.parse(req.body);
+            next();
+
+        }
+        catch(error) {
+
+            // Log the error
+            console.error(`Error occured while validating request body input: ${error}`);
+
+            // Handle a validation error
+
+            if(error instanceof ZodError) {
+
+                const response: IAPIResponse = {
+                    message: 'Input Validation Errors',
+                    code:    StatusCodes.BAD_REQUEST,
+                    errors: error.errors.map(err => ({ field: err.path.join('.'), message: err.message, code: err.code }))
+                };
+
+                res.status(response.code).json(response);
+                return;
+
+            }
+
+            // Handle any other error
+            const response: IAPIResponse = {
+                message: 'Internal Server Error',
+                code:    StatusCodes.INTERNAL_SERVER_ERROR
+            };
+
+            res.status(response.code).json(response);
+            return;
+
+        }
+
+    };
+
+}
