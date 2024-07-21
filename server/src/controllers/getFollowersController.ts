467710fd2f6c46813a632cd369c5db808a157a05
Evan Kasky <ekasky25@gmail.com>
2024-07-21 16:06:55 -0400

    Adds endpoint to get user followers

---
+++ server/src/controllers/getFollowersController.ts
@@ -0,0 +1,54 @@
+import { Request, Response } from 'express';
+import { User } from '../models/User';
+import { IAPIResponse } from '../types/IApiResponse';
+import { Follow } from '../models/Follow';
+
+const getFollowersController = async (req: Request, res: Response) => {
+
+    try {
+
+        // Get the userId from the url
+        const { userId } = req.params;
+
+        // Check if the user exists
+        const user = await User.findById(userId);
+
+        // If the user is not found return a 404
+        if(!user) {
+
+            const response: IAPIResponse = { message: 'User not found', code: 404 };
+            res.status(response.code).json(response);
+            return;
+
+        }
+
+        // Find all the followers of the user
+        const followers = await Follow.find({ following: userId }).populate('follower', 'username');
+
+        // Map the followers to return only id and username
+        const followerData = followers.map(follow => ({
+            id: follow.follower._id,
+            username: follow.follower.username,
+        }));
+
+        // Return a success message and the followers
+        const response: IAPIResponse = { message: 'Followers retrieved successfully', code: 200, data: followers };
+        res.status(response.code).json(response);
+        return;
+
+    }
+
+    catch(error) {
+
+        console.log(`Get Followers Error: ${error}`);
+
+        // Send error response
+        const response: IAPIResponse = { message: 'Internal Server Error', code: 500 };
+        res.status(response.code).json(response);
+        return;
+
+    }
+
+};
+
+export default getFollowersController;
--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -6,11 +6,13 @@ import { UpdateUserProfileSchema } from '../validationSchemas/updateUserProfileS
 import updateUserProfileController from '../controllers/updateUserProfileController';
 import followUserController from '../controllers/auth/followUserController';
 import unfollowUserController from '../controllers/unfollowUserController';
+import getFollowersController from '../controllers/getFollowersController';
 const router = express.Router();
 
 router.get('/users/:userId', getUserInfoController);
 router.put('/users/:userId', isLoggedIn, validateRequestInput(UpdateUserProfileSchema), updateUserProfileController);
 router.post('/users/:userId/follow', isLoggedIn, followUserController);
 router.post('/users/:userId/unfollow', isLoggedIn, unfollowUserController);
+router.get('/users/:userId/followers', getFollowersController);
 
 export default router;
