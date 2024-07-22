23d1abe56644ecaf1a75a52a9957d591b85e1477
Evan Kasky <ekasky25@gmail.com>
2024-07-22 02:17:07 -0400

    Serves static react files

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,10 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import connectDb from './utils/connectDb';
 import authRouter from './routes/authRoutes';
 import createTransporter from './utils/transporter';
 import router from './routes/routes';
+import path from 'path';
 
 // Load the .env variables
 dotenv.config();
@@ -25,7 +26,13 @@ app.use('/api/auth', authRouter);
 app.use('/api', router);
 
 // Serve Static React Files
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
+app.get('*', (req: Request, res: Response): void => {
+
+    res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
+
+});
 
 // Start the server
 const port = process.env.PORT;
