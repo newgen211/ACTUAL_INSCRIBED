495b7cdd13b2647253a1dd3381530d907a09dbd2
Evan Kasky <ekasky25@gmail.com>
2024-07-22 05:02:15 -0400

    Connects new post form to the api

--- client/src/components/NewPostForm.tsx
+++ client/src/components/NewPostForm.tsx
@@ -1,7 +1,94 @@
-import { Avatar, Box, Button, Card, CardActions, CardContent, CardHeader, TextField, Typography } from '@mui/material';
+import { Alert, Avatar, Box, Button, Card, CardActions, CardContent, CardHeader, FormHelperText, Snackbar, TextField, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useAuth } from '../context/AuthContext';
+import axios from 'axios';
 
 
 export default function NewPostForm() {
+
+    /* Define State */
+    const [content, setContent] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError]     = useState<string>('');
+    const [success, setSuccess] = useState<string>('');
+    const { auth } = useAuth();
+
+    /* Handle content change */
+    const handleContentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setContent(event.target.value);
+    };
+
+    /* Handle Submit */
+    const handleSubmit = async () => {
+
+        setLoading(true);
+
+        if(!content.trim()) {
+
+            setError('Post cannot be empty');
+            setLoading(false);
+            return;
+
+        }
+
+        if(content.length > 255) {
+
+            setError('Post cannot be longer than 255 characters');
+            setLoading(false);
+            return;
+
+        }
+
+        try {
+
+            // Get the token from localstorage
+            const token = localStorage.getItem('token');
+
+            // Attempt to create a new post
+            const response = await axios.post('/api/post', 
+                { 
+                    content, 
+                    userId: auth?.userId
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+            );
+
+            setContent('');
+            setSuccess('Post created successfully');
+
+        }
+
+        catch(error) {
+
+            console.error('Error creating post:', error);
+
+        }
+
+        finally {
+
+            setLoading(false);
+
+        }
+
+    };
+
+    /* Auto dismiss the errors */
+    useEffect(() => {
+
+        if (error) {
+            
+            const timer = setTimeout(() => {
+                setError('');
+            }, 5000);
+
+            return () => clearTimeout(timer);
+        }
+
+    }, [error, setError]);
 
     return (
 
@@ -11,13 +98,24 @@ export default function NewPostForm() {
 
             <CardContent>
 
-                <TextField
+            <TextField
                     id="outlined-multiline-flexible"
-                    label="Whats on your mind..."
+                    label="What's on your mind..."
                     multiline
                     fullWidth
                     rows={5}
-                    />  
+                    value={content}
+                    onChange={handleContentChange}
+                    error={!!error}
+                    helperText={error}
+                    InputProps={{
+                        endAdornment: (
+                            <FormHelperText sx={{ position: 'absolute', bottom: -20, right: 0 }}>
+                                {content.length}/255
+                            </FormHelperText>
+                        )
+                    }}
+                />  
 
             </CardContent>
 
@@ -25,9 +123,15 @@ export default function NewPostForm() {
                 
 
                 <Box sx={{ flexGrow: 1 }} />
-                <Button variant='contained'>Post</Button>
+                <Button onClick={handleSubmit} disabled={loading} variant='contained'>{loading ? 'Posting...' : 'Post'}</Button>
 
             </CardActions>
+
+            <Snackbar open={!!success} autoHideDuration={6000} onClose={() => setSuccess('')}>
+                <Alert onClose={() => setSuccess('')} severity="success" sx={{ width: '100%' }}>
+                    {success}
+                </Alert>
+            </Snackbar>
 
         </Card>
 
