aa009ee0cefab7e9815e29dec60feeb54b51bba0
Evan Kasky <ekasky25@gmail.com>
2024-07-22 00:13:58 -0400

    Adds a post component

--- client/src/components/Feed.tsx
+++ client/src/components/Feed.tsx
@@ -1,11 +1,21 @@
 import { Box } from '@mui/material';
+import Post from './Post';
 
 
 export default function Feed() {
 
     return (
 
-        <Box bgcolor='pink' flex={4} p={2}>Feed</Box>
+        <Box flex={4} p={2}>
+
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+
+        </Box>
 
     );
 
