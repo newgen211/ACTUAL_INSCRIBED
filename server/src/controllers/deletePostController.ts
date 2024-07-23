971026f5d4832cc7be2ca86aa919551673c23b5f
Evan Kasky <ekasky25@gmail.com>
2024-07-22 21:56:51 -0400

    Fixes delete post

--- server/src/controllers/deletePostController.ts
+++ server/src/controllers/deletePostController.ts
@@ -6,11 +6,9 @@ import { Comment } from '../models/Comment';
 import { Like } from '../models/Like';
 
 const deletePostController = async (req: Request, res: Response) => {
-
     try {
-
         const { postId } = req.params;
-        
+
         // Convert postId to ObjectId
         const postObjectId = new mongoose.Types.ObjectId(postId);
 
@@ -19,49 +17,39 @@ const deletePostController = async (req: Request, res: Response) => {
 
         // Return a 404 if the post is not found
         if (!post) {
-
             const response: IAPIResponse = { message: 'Post not found', code: 404 };
             res.status(response.code).json(response);
             return;
-
         }
 
         // Check if the user is the owner of the post
         if (post.user.toString() !== req.user.userId) {
-
             const response: IAPIResponse = { message: 'Unauthorized to delete this post', code: 403 };
             res.status(response.code).json(response);
             return;
-
         }
 
-        // Delete all posts created by the user
-        await Post.deleteMany({ user: req.user.userId });
+        // Delete the specific post
+        await Post.findByIdAndDelete(postObjectId);
 
-        // Delete all likes made by the user
-        await Like.deleteMany({ user: req.user.userId });
+        // Delete likes associated with the post
+        await Like.deleteMany({ postId: postObjectId });
 
-        // Delete all comments made by the user
-        await Comment.deleteMany({ user: req.user.userId });
+        // Delete comments associated with the post
+        await Comment.deleteMany({ postId: postObjectId });
 
         // Return a success response
         const response: IAPIResponse = { message: 'Post deleted successfully', code: 200 };
         res.status(response.code).json(response);
         return;
-
-    }
-
-    catch(error) {
-
+    } catch (error) {
         console.log(`Delete Post Error: ${error}`);
 
         // Send error response
         const response: IAPIResponse = { message: 'Internal Server Error', code: 500 };
         res.status(response.code).json(response);
         return;
-
     }
-
 };
 
 export default deletePostController;
